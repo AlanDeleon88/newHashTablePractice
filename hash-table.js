0a254f04206bd86efc194ccef0d4a6e54916140f
AlanDeleon88 <19deleon88@gmail.com>
2023-07-12 17:56:12 -0700

    refactored a bit

--- hash-table.js
+++ hash-table.js
@@ -11,27 +11,86 @@ class KeyValuePair {
 class HashTable {
 
   constructor(numBuckets = 4) {
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets
+    this.data = new Array(numBuckets).fill(null)
+
+
   }
 
   hash(key) {
-    // Your code here
+    let hash = sha256(key).slice(0,8)
+
+    return parseInt(hash, 16)
   }
 
   hashMod(key) {
-    // Your code here
+
+    return this.hash(key) % this.capacity
   }
 
   insertNoCollisions(key, value) {
-    // Your code here
+    let newPair = new KeyValuePair(key, value)
+    if(this.data[this.hashMod(key)]) throw new Error('hash collision or same key/value pair already exists!')
+    this.data[this.hashMod(key)] = newPair
+    this.count++
+
   }
 
   insertWithHashCollisions(key, value) {
-    // Your code here
+    let newPair = new KeyValuePair(key, value)
+    let hashKey = this.hashMod(key)
+
+    if(this.data[hashKey]){
+      let current = this.data[hashKey]
+      newPair.next = current
+      this.data[hashKey] = newPair
+      this.count++
+
+    }
+    else{
+      this.data[hashKey] = newPair
+      this.count++
+    }
+
+
+  }
+
+  handleCollision(key,value, pair, hashKey){
+
+    let current = this.data[hashKey]
+    let head = this.data[hashKey]
+
+
+    while(current && current.key !== key){
+
+      current = current.next
+    }
+    if(current){
+      current.value = value
+    }
+    else{
+      pair.next = head
+      this.data[hashKey] = pair
+      this.count++
+    }
+
   }
 
   insert(key, value) {
-    // Your code here
+    let newPair = new KeyValuePair(key, value)
+    let hashKey = this.hashMod(key)
+
+    if(this.data[hashKey]){
+      this.handleCollision(key,value, newPair, hashKey)
+
+    }
+    else{
+      this.data[hashKey] = newPair
+      this.count++
+
+    }
+
   }
 
 }
